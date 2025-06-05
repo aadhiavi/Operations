05c438626e6bb948a030af3c39fd30f133d80326
Kurapati Avinash <aadhiavi57@outlook.com>
2025-06-05 12:02:22 +0530

    some files added

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ const { startBirthdayReminder } = require('./cron/birthdayReminder');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'https://ops.tradesyndicate.in',
-  credentials: true
-}));
+app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(session({
   secret: process.env.SESSION_SECRET,
